896b96fb853e9de3b4c6a6aaf94cd356141ed1d4
igatecomputereducation <igatecomputereducation@gmail.com>
2024-02-27 11:10:57 +0530

    v1

---
+++ spring-mvc-project/spring-mvc-project/src/main/java/com/igate/Products.java
@@ -0,0 +1,32 @@
+package com.igate;
+public class Products {
+	private int pcode;
+	private String pname;
+	private float price;
+	private int stock;
+	public int getPcode() {
+		return pcode;
+	}
+	public void setPcode(int pcode) {
+		this.pcode = pcode;
+	}
+	public String getPname() {
+		return pname;
+	}
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	
+}
